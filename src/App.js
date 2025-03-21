5d5f85ac6c9b04c9d7ecf973c0797752ec4a335e
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-03-21 14:48:11 -0700

    Se agregó el mapa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import './App.scss';
 import image from "../src/images/pattern-bg-desktop.png";
 import iconArrow from "../src/images/icon-arrow.svg"
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import { useState, useEffect } from 'react'
 
 function App() {
   const [location, setLocation] = useState(null);
   const apiKey = process.env.REACT_APP_API_KEY;
+  const position = [51.505, -0.09];
 
   useEffect(() => {
-    fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=8.8.8.8`)
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=8.8.8.8`)
       .then((res) => res.json())
       .then((data) => {
-        if(data.code){
+        if (data.code) {
           throw Error("MENSAJE DE ERROR")
         }
         setLocation(data)
       })
       .catch((error) => console.error("Error obteniendo la información:", error));
   }, []);
-console.log("error location", location)
+  console.log("error location", location)
 
   return (
     <div className="App">
@@ -38,10 +40,14 @@ console.log("error location", location)
           </div>
           <div className="col-sm-12 col-md-3">
             <h4>LOCATION</h4>
-            <h3>{location && location.location && location.location.country ? location.location.country: "--"} {location ? location.location.region:"--"}</h3>
+            {/* Se puede validar que existan las propiedades de esta manera.
+            Se pregunta si tiene la propiedad por niveles. */}
+            <h3>{location && location.location && location.location.region ? location.location.region : "--"}, {location ? location.location.city : "--"}</h3>
           </div>
           <div className="col-sm-12 col-md-3">
             <h4>TIMEZONE</h4>
+            {/* Se puede validar que existan las propiedades de esta manera.
+            Se pregunta si existe cada propiedad */}
             <h3>{location?.location?.timezone ? location.location.timezone : "--"}</h3>
           </div>
           <div className="col-sm-12 col-md-3">
@@ -52,11 +58,20 @@ console.log("error location", location)
       </div>
       <div>
         <div className='map-container'>
-          {/* <LoadScript googleMapsApiKey="AIzaSyDrC01_CcLy_TUB6Yd4ncjZcpV-5sQrbZo">
-            <GoogleMap mapContainerClassName='google-map-container' center={location} zoom={14}>
-              <Marker position={location} />
-            </GoogleMap>
-          </LoadScript> */}
+        <MapContainer className='map-template' center={position} zoom={13} style={{ height: '700px', width: '100%', zIndex:'1' }}>
+      {/* Capa de tiles de OpenStreetMap */}
+      <TileLayer
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {/* Marcador en el mapa */}
+      <Marker position={position}>
+        {/* Popup con texto */}
+        <Popup>
+          A pretty CSS popup.<br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
         </div>
       </div>
     </div>
