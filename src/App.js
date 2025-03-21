9986c6902c0a11aa460871c60edf72b3af0ea7d7
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-03-21 13:27:55 -0700

    Se hizo la petición a la api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.scss';
 import image from "../src/images/pattern-bg-desktop.png";
 import iconArrow from "../src/images/icon-arrow.svg"
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import { useState, useEffect } from 'react'
 
-
-
 function App() {
+  const [location, setLocation] = useState(null);
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=8.8.8.8`)
+      .then((res) => res.json())
+      .then((data) => {
+        if(data.code){
+          throw Error("MENSAJE DE ERROR")
+        }
+        setLocation(data)
+      })
+      .catch((error) => console.error("Error obteniendo la información:", error));
+  }, []);
+console.log("error location", location)
 
   return (
     <div className="App">
@@ -20,25 +34,30 @@ function App() {
         <div className="row ip-address-information">
           <div className="col-sm-12 col-md-3">
             <h4>IP ADDRESS</h4>
-            <h3>192.212.174.101</h3>
+            <h3>{location && location.ip ? location.ip : "--"}</h3>
           </div>
           <div className="col-sm-12 col-md-3">
             <h4>LOCATION</h4>
-            <h3>Brooklyn, NY 10001</h3>
+            <h3>{location && location.location && location.location.country ? location.location.country: "--"} {location ? location.location.region:"--"}</h3>
           </div>
           <div className="col-sm-12 col-md-3">
             <h4>TIMEZONE</h4>
-            <h3>UTC-05:00</h3>
+            <h3>{location?.location?.timezone ? location.location.timezone : "--"}</h3>
           </div>
           <div className="col-sm-12 col-md-3">
             <h4>ISP</h4>
-            <h3>SpaceX Starlink</h3>
+            <h3>{location ? location.isp : "--"}</h3>
           </div>
         </div>
       </div>
       <div>
-        <div style={{ with: '100vw', height: '700px', backgroundColor: 'blueviolet' }}></div>
-      
+        <div className='map-container'>
+          {/* <LoadScript googleMapsApiKey="AIzaSyDrC01_CcLy_TUB6Yd4ncjZcpV-5sQrbZo">
+            <GoogleMap mapContainerClassName='google-map-container' center={location} zoom={14}>
+              <Marker position={location} />
+            </GoogleMap>
+          </LoadScript> */}
+        </div>
       </div>
     </div>
   );
