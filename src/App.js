11da95b0d8dcbb5c1a08b921a629673a5802de8c
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-03-25 15:41:46 -0700

    Se actualiza la información del mapa con cada busqueda.

--- src/App.js
+++ src/App.js
@@ -3,25 +3,38 @@ import image from "../src/images/pattern-bg-desktop.png";
 import iconArrow from "../src/images/icon-arrow.svg"
 
 
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useState, useEffect } from 'react'
+
+const MapUpdater = ({ lat, lng }) => {
+  const map = useMap();
+  useEffect(() => {
+    if (lat && lng) {
+      map.setView([lat, lng], 13); // Centrar mapa
+    }
+  }, [lat, lng, map]);
+  return null;
+}
 
 function App() {
   const [location, setLocation] = useState(null);
   const apiKey = process.env.REACT_APP_API_KEY;
-  const position = [51.505, -0.09];
-
-
-  const [input, setInput] = useState('8.8.8.8')
+  const [position, setPosition] = useState({ lat: 51.505, lng: -0.09 });
+  const [input, setInput] = useState('')
 
 
   useEffect(() => {
     getIpInfo()
   }, []);
 
+  console.log(position)
+
+
   const getIpInfo = () => {
-    const option = isIP(input) ? `&ipAddress=${input}` : `&domain=${input}`
+    const search = input=='' ? 'www.starlink.com' : input
+
+    const option = isIP(search) ? `&ipAddress=${search}` : `&domain=${search}`
     fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}${option}`)
       .then((res) => res.json())
       .then((data) => {
@@ -29,6 +42,10 @@ function App() {
           throw Error("MENSAJE DE ERROR")
         }
         setLocation(data)
+        console.log(data)
+        setPosition({ lat: data.location.lat, lng: data.location.lng });
+
+
       })
       .catch((error) => console.error("Error obteniendo la información:", error));
   }
@@ -46,16 +63,25 @@ function App() {
     return false
   }
 
+  const submit = (event) => {
+    event.preventDefault();
+    console.log("se dio enter");
+  };
+
+
   return (
     <div className="App">
       <div className='header'>
         <h3 className='title'>IP Address Tracker</h3>
-        <div class="input-group mb-3">
-          <input onChange={changeInput} value={input} type="text" class="form-control" placeholder="Search for any IP address of domain" aria-label="Recipient's username" aria-describedby="button-addon2" ></input>
-          <button onClick={getIpInfo} class="btn btn-outline-secondary" type="button" id="button-addon2">
-            <img src={iconArrow}></img>
-          </button>
-        </div>
+        <form onSubmit={submit}>
+          <div class="input-group mb-3">
+            <input onChange={changeInput} value={input} type="text" class="form-control" placeholder="Search for any IP address of domain" aria-label="Recipient's username" aria-describedby="button-addon2" ></input>
+            <button onClick={getIpInfo} class="btn btn-outline-secondary" type="submit" id="button-addon2">
+              <img src={iconArrow}></img>
+            </button>
+          </div>
+        </form>
+
         <div className="row ip-address-information">
           <div className="col-sm-12 col-md-3">
             <h4>IP ADDRESS</h4>
@@ -81,19 +107,14 @@ function App() {
       </div>
       <div>
         <div className='map-container'>
-          <MapContainer className='map-template' center={position} zoom={14} style={{ height: '700px', width: '100%', zIndex: '1' }}>
-            {/* Capa de tiles de OpenStreetMap */}
+          <MapContainer className='map-template' center={[position.lat, position.lng]} zoom={16} style={{ height: '740px', width: '100%', zIndex: '1' }}>
             <TileLayer
-              url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-            {/* Marcador en el mapa */}
-            <Marker position={position}>
-              {/* Popup con texto */}
-              <Popup>
-                A pretty CSS popup.<br /> Easily customizable.
-              </Popup>
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[position.lat, position.lng]}>
+              <Popup> Estas aqui</Popup>
             </Marker>
+            <MapUpdater lat={position.lat} lng={position.lng} />
           </MapContainer>
         </div>
       </div>
